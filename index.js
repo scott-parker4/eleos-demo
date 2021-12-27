5ebf94d6dd8ce2f4fa749fde9c1a720a358948fb
scott p <scottparkerdesign@gmail.com>
2021-12-27 14:07:07 -0500

    change PORT varible name

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('authenticate/:token', (req, res) => {
         });
 })
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
