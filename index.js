db25b24a74e92cf8a7471b76c422a39970785337
scott p <scottparkerdesign@gmail.com>
2021-12-28 13:41:48 -0500

    minor auth fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt_encode = require('jwt-encode')
 
 const app = express()
 
-app.use(express.json()); //Used to parse JSON bodies
+app.use(express.json()); // Used to parse JSON bodies
 
 const url = `mongodb+srv://eleosadmin:${process.env.MONGO_PASS}@cluster0.uylzw.mongodb.net/Eleos?retryWrites=true&w=majority`
 
@@ -23,10 +23,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/authenticate/:token', (req, res) => {
-    //const jwtDecode = jwt_decode(req.params.token)
-    User.findOne({ username: jwt_decode(req.params.token) })
+    const jwtDecode = jwt_decode(req.params.token)
+    User.findOne({ username: jwtDecode["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] })
         .then((user) => {
-            //user.api_token = jwt_encode({ "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": user.username, "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": user.full_name }, 'secret')
+            // user.api_token = jwt_encode({ "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": user.username, "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": user.full_name }, 'secret')
             res.json(user)
             console.log(user)
         }).catch((err) => {
