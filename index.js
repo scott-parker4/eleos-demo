221d711e783821ba4a00af800c8b9f5269fddf1b
scott p <scottparkerdesign@gmail.com>
2021-12-28 11:03:01 -0500

    add jwt encode to auth endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const jwt_decode = require('jwt-decode')
+const jwt_encode = require('jwt-encode')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.get('/authenticate/:token', (req, res) => {
     const jwtDecode = jwt_decode(req.params.token)
     User.findOne({ full_name: jwtDecode["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] })
         .then((user) => {
+            user.api_token = jwt_encode({ "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": user.username, "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": user.full_name }, 'secret')
             res.json(user)
             console.log(user)
         }).catch((err) => {
